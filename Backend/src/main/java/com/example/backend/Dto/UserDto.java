92068bd9629ce4cde1a72303006654370ef119b6
maryam mrg <maryamelouadaa@gmail.com>
2025-07-04 17:19:41 +0100

    creating UserDto

---
+++ Backend/src/main/java/com/example/backend/Dto/UserDto.java
@@ -0,0 +1,53 @@
+package com.example.backend.Dto;
+
+import com.example.backend.Model.Role;
+
+public class UserDto {
+
+
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+
+    public UserDto(String name, String email, String password, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public UserDto() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
