8b473a91678b738215f157bea624a0b1ad9950f4
maryam mrg <maryamelouadaa@gmail.com>
2025-07-07 15:09:41 +0100

    creating User Mapper

---
+++ Backend/src/main/java/com/example/backend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.example.backend.mapper;
+
+import com.example.backend.Dto.UserDto;
+import com.example.backend.Model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.control.MappingControl;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    User toEntity(UserDto userDto);
+
+    UserDto toDto(User user);
+
+    List<UserDto> toDtos(List<User> users);
+}
