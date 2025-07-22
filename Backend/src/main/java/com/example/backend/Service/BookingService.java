c7c3202a7644650634bec1c40d3be927d7fd69b2
maryam mrg <maryamelouadaa@gmail.com>
2025-07-22 09:47:48 +0100

    implimenting booking service

--- Backend/src/main/java/com/example/backend/Service/BookingService.java
+++ Backend/src/main/java/com/example/backend/Service/BookingService.java
@@ -1,7 +1,48 @@
 package com.example.backend.Service;
 
+import com.example.backend.Dto.BookingDto;
+import com.example.backend.Model.Booking;
+import com.example.backend.mapper.BookingMapper;
+import com.example.backend.repository.BookingRepository;
+import com.example.backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingService {
+
+    private final BookingMapper bookingMapper;
+    private final BookingRepository bookingRepository;
+    private final UserRepository userRepository;
+
+    public BookingService(BookingMapper bookingMapper, BookingRepository bookingRepository, UserRepository userRepository) {
+        this.bookingMapper = bookingMapper;
+        this.bookingRepository = bookingRepository;
+        this.userRepository = userRepository;
+    }
+
+    public BookingDto addBooking(BookingDto bookingDto){
+        Booking booking = bookingMapper.toEntity(bookingDto);
+
+        Booking saved = bookingRepository.save(booking);
+        return bookingMapper.toDto(saved);
+    }
+
+    public List<BookingDto> getAll(){
+        List<Booking> bookings = bookingRepository.findAll();
+        return bookingMapper.toDtos(bookings);
+    }
+
+    public BookingDto updateBooking(BookingDto dto,Long id){
+        Booking booking =bookingRepository.findById(id).orElseThrow(()->new RuntimeException("not found"));
+        booking.setEndTime(dto.getEndTime());
+        booking.setStartTime(dto.getStartTime());
+        Booking saved =bookingRepository.save(booking);
+        return bookingMapper.toDto(saved);
+    }
+
+    public void deleteBooking(Long id){
+        bookingRepository.deleteById(id);
+    }
 }
