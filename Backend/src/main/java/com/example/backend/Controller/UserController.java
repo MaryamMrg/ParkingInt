663f5e0f933b2c16cd0ebf82eea7a6a7cd87a02a
maryam mrg <maryamelouadaa@gmail.com>
2025-07-24 17:09:22 +0100

    implimenting User controller

---
+++ Backend/src/main/java/com/example/backend/Controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.backend.Controller;
+
+import com.example.backend.Dto.UserDto;
+import com.example.backend.Service.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/all")
+    public List<UserDto> getUsers() {return userService.getUsers();}
+
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+
+
+    @PutMapping("update/{id}")
+    public UserDto updateUser(@RequestBody UserDto userDto, @PathVariable Long id) {
+        return userService.updateUser(userDto, id);
+    }
+}
