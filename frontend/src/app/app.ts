801a92b1686e860db154419c6182c91e1304663b
maryam mrg <maryamelouadaa@gmail.com>
2025-07-31 17:24:34 +0100

    impliment the home component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav-component/nav-component";
+import { HomeComponent } from "./home-component/home-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
