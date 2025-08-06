bde57dd366210b27d30c777381ece0036f06de2a
maryam mrg <maryamelouadaa@gmail.com>
2025-08-06 23:03:07 +0100

    implimenting customer dashboard

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,10 +4,11 @@ import { NavComponent } from "./nav-component/nav-component";
 import { HomeComponent } from "./home-component/home-component";
 import { LoginComponent } from "./login-component/login-component";
 import { Signup } from './signup/signup';
+import { CustomerDashboard } from "./customer-dashboard/customer-dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, HomeComponent, LoginComponent,Signup],
+  imports: [RouterOutlet, NavComponent, HomeComponent, LoginComponent, Signup, CustomerDashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- frontend/src/app/customer-dashboard/customer-dashboard.ts
+++ frontend/src/app/customer-dashboard/customer-dashboard.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Authservice } from '../authservice';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-dashboard',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './customer-dashboard.html',
   styleUrl: './customer-dashboard.css'
 })
-export class CustomerDashboard {
+export class CustomerDashboard implements OnInit {
+customername : String ='';
+  constructor(private authservice : Authservice,
+     private router : Router)
+     { }
 
+
+  ngOnInit(): void {
+    this.loadCustomerData();
+  }
+
+  loadCustomerData() : void{
+const user = this.authservice.getCurrentUser();
+    this.customername = user?.name || 'Customer';
+  }
 }
