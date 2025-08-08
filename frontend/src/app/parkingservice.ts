40cd936ff6988a51dd0fa5abfb329d8ec7fbe29c
maryam mrg <maryamelouadaa@gmail.com>
2025-08-08 11:28:10 +0100

    implimenting the parking service methods

--- frontend/src/app/parkingservice.ts
+++ frontend/src/app/parkingservice.ts
@@ -1,9 +1,48 @@
+import { T } from '@angular/cdk/keycodes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+
+export interface Parking{
+ parking_Id?:number;
+ P_name: string;
+ capacity: number;
+ avaible_places: number;
+ opening_hours: number
+
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class Parkingservice {
 
-  constructor() { }
+  private apiurl="http://localhost:8080/parking";
+
+  constructor(private http : HttpClient) { }
+
+  getAuthHeader():HttpHeaders{
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({ 'Content-Type': 'application/json',
+    'Authorization': token ? `Bearer ${token}` : ''});
+  }
+
+  getAllPArkings() : Observable<any>{
+    return this.http.get<any>(`${this.apiurl}/all`,{headers:this.getAuthHeader()});
+  }
+
+  createParking(parking:Parking){
+    return this.http.post<Parking>(`${this.apiurl}/add`,parking,{headers:this.getAuthHeader()});
+  }
+
+  updatePArking(id :number,parking:Parking){
+    return this.http.put<Parking>(`${this.apiurl}/update/${id}`,parking,{headers : this.getAuthHeader()});
+  }
+
+  deletParking(id:number):Observable<any>{
+    return this.http.delete(`${this.apiurl}/${id}`,{headers:this.getAuthHeader()});
+  }
 }
