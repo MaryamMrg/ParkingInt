c0b366f4c01883606a79a55389850cb633b0d5a2
maryam mrg <maryamelouadaa@gmail.com>
2025-08-15 20:45:14 +0100

    add cross origin for the booking controller

--- Backend/src/main/java/com/example/backend/Controller/BookingController.java
+++ Backend/src/main/java/com/example/backend/Controller/BookingController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/booking")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BookingController {
 
     private final BookingService bookingService;
