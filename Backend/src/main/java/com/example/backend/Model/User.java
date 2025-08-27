78801cc25ef85a2cf775f0ee0ca0a0683c600524
maryam mrg <maryamelouadaa@gmail.com>
2025-08-27 23:30:59 +0100

    updates on role in user entity

--- Backend/src/main/java/com/example/backend/Model/User.java
+++ Backend/src/main/java/com/example/backend/Model/User.java
@@ -19,6 +19,8 @@ public class User implements UserDetails {
     private String name;
     private String email;
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     public User(Long userId, String name, String email, String password, Role role) {
